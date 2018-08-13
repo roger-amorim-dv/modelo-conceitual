22944b5391faf68dd5e44311dcaaef8ee6855646
roger-amorim-dv <rogertc16@gmail.com>
2018-08-12 23:31:25 -0300

    Testando Dominio

--- src/main/java/br/com/modeloconceitual/resource/CategoriaResource.java
+++ src/main/java/br/com/modeloconceitual/resource/CategoriaResource.java
@@ -1,15 +1,29 @@
 package br.com.modeloconceitual.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.modeloconceitual.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!!!";
+	/*@RequestMapping(method = RequestMethod.GET)*/
+	@GetMapping
+	public List<Categoria> listar() {
+		
+		Categoria cat01 = new Categoria(1, "Informática");
+		Categoria cat02 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat01);
+		lista.add(cat02);
+		
+		return lista;
 	}
 }
