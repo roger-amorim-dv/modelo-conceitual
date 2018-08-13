79b575069a5e1989dc07e9f774a66c30f66de37e
roger-amorim-dv <rogertc16@gmail.com>
2018-08-13 00:59:43 -0300

    Instanciar objeto direto ao buildar o H2

--- src/main/java/br/com/modeloconceitual/McApplication.java
+++ src/main/java/br/com/modeloconceitual/McApplication.java
@@ -1,12 +1,30 @@
 package br.com.modeloconceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.modeloconceitual.domain.Categoria;
+import br.com.modeloconceitual.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class McApplication {
+public class McApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat01 = new Categoria(null, "Informática");
+		Categoria cat02 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat01, cat02));
 	}
 }
