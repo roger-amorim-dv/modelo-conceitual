69cc5710fcfe0d2ae6e53f7c4b2f5195f830202c
roger-amorim-dv <rogertc16@gmail.com>
2018-08-13 01:35:38 -0300

    Associação muitos para muitos, macete do addAll

--- src/main/java/br/com/modeloconceitual/McApplication.java
+++ src/main/java/br/com/modeloconceitual/McApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.modeloconceitual.domain.Categoria;
+import br.com.modeloconceitual.domain.Produto;
 import br.com.modeloconceitual.repositories.CategoriaRepository;
+import br.com.modeloconceitual.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class McApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
@@ -25,6 +30,19 @@ public class McApplication implements CommandLineRunner{
 
 		Categoria cat01 = new Categoria(null, "Informática");
 		Categoria cat02 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat01.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat02.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat01));
+		p2.getCategorias().addAll(Arrays.asList(cat01, cat02));
+		p3.getCategorias().addAll(Arrays.asList(cat01));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat01, cat02));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 }
