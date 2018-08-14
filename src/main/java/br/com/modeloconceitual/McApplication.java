d161c8ac815a659ea84e439a0d8985321de374b7
roger-amorim-dv <rogertc16@gmail.com>
2018-08-14 00:27:24 -0300

    Estado e Cidade

--- src/main/java/br/com/modeloconceitual/McApplication.java
+++ src/main/java/br/com/modeloconceitual/McApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.modeloconceitual.domain.Categoria;
+import br.com.modeloconceitual.domain.Cidade;
+import br.com.modeloconceitual.domain.Estado;
 import br.com.modeloconceitual.domain.Produto;
 import br.com.modeloconceitual.repositories.CategoriaRepository;
+import br.com.modeloconceitual.repositories.CidadeRepository;
+import br.com.modeloconceitual.repositories.EstadoRepository;
 import br.com.modeloconceitual.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class McApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
@@ -42,7 +52,19 @@ public class McApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat01, cat02));
 		p3.getCategorias().addAll(Arrays.asList(cat01));
 		
+		Estado est01 = new Estado(null, "Minas Gerais");
+		Estado est02 = new Estado(null, "São Paulo");
+		
+		Cidade c01 = new Cidade(null, "Uberlândia", est01);
+		Cidade c02 = new Cidade(null, "São Paulo", est02);
+		Cidade c03 = new Cidade(null, "Campinas", est02);
+		
+		est01.getCidades().addAll(Arrays.asList(c01));
+		est02.getCidades().addAll(Arrays.asList(c02, c03));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat01, cat02));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepository.saveAll(Arrays.asList(est01, est02));
+		cidadeRepository.saveAll(Arrays.asList(c01, c02, c03));
 	}
 }
