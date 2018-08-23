20b0591ea3bae9ad407bb447104d1620b254c0d8
roger-amorim-dv <rogertc16@gmail.com>
2018-08-23 00:55:59 -0300

    End-point PUT de Categoria

--- src/main/java/br/com/modeloconceitual/services/CategoriaService.java
+++ src/main/java/br/com/modeloconceitual/services/CategoriaService.java
@@ -25,4 +25,8 @@ public class CategoriaService {
 		 obj.setId(null);
 		 return repo.save(obj);
 	 }
+	 
+	 public Categoria update(Categoria obj) {
+		 find(obj.getId());
+		 return repo.save(obj);	 }
 }
