eada0c917b424a6b598c3b4d551a9de4d821e80c
roger-amorim-dv <rogertc16@gmail.com>
2018-08-23 00:21:10 -0300

    end-point POST de Categoria

--- src/main/java/br/com/modeloconceitual/services/CategoriaService.java
+++ src/main/java/br/com/modeloconceitual/services/CategoriaService.java
@@ -15,9 +15,14 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFundException(
 				"Objeto não encontrado! ID: " + id + ", tipo: " + Categoria.class.getName()));
 	}
+	
+	 public Categoria insert(Categoria obj) {
+		 obj.setId(null);
+		 return repo.save(obj);
+	 }
 }
